30ad3b944284a4bb40d5dd870c385a73ead74646
Ralf Kaestner <ralf.kaestner@gmail.com>
2009-08-12 14:34:17 +0000

    uBash packaging with ReMake

---
+++ src/lib/global.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+############################################################################
+#    Copyright (C) 2007 by Ralf 'Decan' Kaestner                           #
+#    ralf.kaestner@gmail.com                                               #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+# Global functions
+
+function define {
+  VAR=$1
+  shift
+  unset $VAR
+
+  if [ $# -gt 1 ]; then
+    while [ -n "$1" ]; do
+      eval "$VAR[\${#$VAR[*]}]=\"$1\""
+      shift
+    done
+  else
+    eval "$VAR=\"$1\""
+  fi
+}
+
+function defined {
+  [ "${!1-X}" == "${!1-Y}" ]
+}
+
+function true {
+  [ "${!1}" == "true" ] || [ "${!1}" == "yes" ]
+}
+
+function false {
+  [ "${!1}" != "true" ] && [ "${!1}" != "yes" ]
+}
+
+function keyval
+{
+  eval $2=`echo $1 | grep -o "^[^=]*"`
+  eval $3=`echo $1 | grep -o "=.*$" | sed s/"^="//`
+}
+
+function include
+{
+  INCSOURCE=${BASH_SOURCE[1]}
+  [ "$0" == "$INCSOURCE" ] && INCDIR=`pwd` || INCDIR=`dirname $INCSOURCE`
+
+  while [ -n "$1" ]; do
+    [[ "$1" =~ ^/ ]] && INCLUDE="$1" || INCLUDE="$INCDIR/$1"
+
+    if [ -r "$INCLUDE" ]; then
+      . "$INCLUDE"
+      INCLUDES=("$INCLUDE" ${INCLUDES[*]})
+    else
+      message_exit "missing include $1"
+    fi
+
+    shift
+  done
+}
+
+include "system.sh"
+include "archive.sh"
+include "array.sh"
+include "build.sh"
+include "execute.sh"
+include "fs.sh"
+include "install.sh"
+include "log.sh"
+include "math.sh"
+include "message.sh"
+include "network.sh"
+include "regexp.sh"
+include "script.sh"
+include "string.sh"
+include "test.sh"
+include "xen.sh"
